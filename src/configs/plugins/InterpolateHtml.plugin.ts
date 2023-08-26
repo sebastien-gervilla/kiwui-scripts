c84c34f773c40c6acee458ebfff7500861ea68f9
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-08-26 15:12:53 +0200

    Fix: Properly interpolating html

--- src/configs/plugins/InterpolateHtml.plugin.ts
+++ src/configs/plugins/InterpolateHtml.plugin.ts
@@ -1,27 +1,24 @@
 import escape from 'escape-string-regexp';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { Environment } from '../../types/config';
+import { Compilation, Compiler } from 'webpack';
 
 export default class InterpolateHtmlPlugin {
     constructor(public replacements: Environment) {}
 
-    apply(compiler: any) {
-        compiler.hooks.compilation.tap('InterpolateHtmlPlugin', (compilation: any) => {
+    apply(compiler: Compiler) {
+        compiler.hooks.compilation.tap('InterpolateHtmlPlugin', (compilation: Compilation) => {
             HtmlWebpackPlugin
                 .getHooks(compilation)
                 .afterTemplateExecution.tap('InterpolateHtmlPlugin', data => {
                     // Makes environment variables available in HTML.
-                    let replacedHTML = data.html;
                     for (const key in this.replacements)
-                        replacedHTML.replace(
+                        data.html = data.html.replace(
                             new RegExp('%' + escape(key) + '%', 'g'),
                             this.replacements[key]
                         );
 
-                    return {
-                        ...data,
-                        html: replacedHTML
-                    };
+                    return data;
                 });
         });
     }
--- src/utils/getApplicationEnvironment.ts
+++ src/utils/getApplicationEnvironment.ts
@@ -13,7 +13,9 @@ export const getApplicationEnvironment = () => {
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
-        PUBLIC_URL: publicUrl,
+        // We remove the trailing slash because %PUBLIC_URL%/stuff looks better
+        // than %PUBLIC_URL%stuff
+        PUBLIC_URL: publicUrl.slice(0, -1),
     };
 
     for (const key in process.env)
