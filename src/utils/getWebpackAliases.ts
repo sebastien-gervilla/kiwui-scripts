3be20f508185c9a03e0ee61367905abe35f446c5
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-28 11:51:02 +0200

    Feat: Added basic aliases

--- src/utils/getWebpackAliases.ts
+++ src/utils/getWebpackAliases.ts
@@ -3,11 +3,24 @@ import { paths } from "../configs/paths.config";
 import { ProjectConfig } from "../types/config";
 
 /**
- * Gets webpack aliases from the compilerOptions.
+ * Gets webpack aliases the compilerOptions.
 */
-export const getWebpackAliases = (options: Props = {}): ProjectConfig['webpackAliases'] => {
-    const baseUrl = options.baseUrl;
-  
+export const getWebpackAliases = (options: Props): ProjectConfig['webpackAliases'] => {
+    const aliases = getWebpackBaseUrlAlias(options.baseUrl);
+
+    if (!paths)
+        return aliases;
+
+    for (const alias in options.paths)
+        aliases[trimAlias(alias)] = path.resolve(
+            paths.root, 
+            trimAlias(options.paths[alias][0])
+        );
+
+    return aliases;
+}
+
+const getWebpackBaseUrlAlias = (baseUrl: string | undefined): ProjectConfig['webpackAliases'] => {
     if (!baseUrl)
         return {};
   
@@ -18,6 +31,15 @@ export const getWebpackAliases = (options: Props = {}): ProjectConfig['webpackAl
         : {}
 }
 
+const trimAlias = (alias: string) => {
+    return alias.endsWith('/*')
+        ? alias.slice(0, -2)
+        : alias
+}
+
 interface Props {
     baseUrl?: string
+    paths?: {
+        [alias: string]: [string]
+    }
 }
